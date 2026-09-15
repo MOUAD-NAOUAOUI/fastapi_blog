a7bd4d753cc530c82b3c9d49ce902b3b930f4bdf
SAsHa-ux-code <naouaouimouad57@gmail.com>
2026-09-15 18:58:58 +0100

    starting with sqlalchemy

--- schemas.py
+++ schemas.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+from datetime import datetime
 
+class UserBase(BaseModel):
+    username:str=Field(min_length=1,max_length=50)
+    email: EmailStr=Field(max_length=120)
+
+    model_config=ConfigDict(from_attributes=True)
+class UserCreate(UserBase):
+    pass
+class UserResponse(UserBase):
+    model_config=ConfigDict(from_attributes=True)
+    id:int 
+    image_file:str|None
+    image_path:str
 
 class PostBase(BaseModel):
     title:str=Field(min_length=1,max_length=100)
